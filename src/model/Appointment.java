9fd03d322eb9624957e238a83f3884efc90ace99
Jakob Svemar <jakob_svemar@hotmail.com>
2016-04-08 10:27:27 +0200

    added initial code for appointment and database altered to appointment database as well as created test class

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -1,9 +1,38 @@
 package model;
 
 import java.util.Date;
+import java.util.HashSet;
 
-public class Appointment {
+public class Appointment implements Comparable<Appointment>{
 	private Date start;
 	private Date finish;
+	private HashSet<String> participants;
+	
+	public Appointment(Date start, Date finish, String id){
+		this.start = start;
+		this.finish = finish;
+		participants = new HashSet<String>();
+		participants.add(id);
+	}
+	
+	public boolean addParticipant(String id){
+		if(participants.contains(id))
+			return false;
+		
+		participants.add(id);
+		return true;
+	}
+	
+	public Date getStartDate(){
+		return start;
+	}
+	
+	public Date getFinishDate(){
+		return finish;
+	}
 
+	@Override
+	public int compareTo(Appointment o) {
+		return start.toString().compareTo(o.getStartDate().toString());
+	}
 }
---
+++ src/modelTest/AppointmentDatabaseTest.java
@@ -0,0 +1,16 @@
+package modelTest;
+
+import model.*;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppointmentDatabaseTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
