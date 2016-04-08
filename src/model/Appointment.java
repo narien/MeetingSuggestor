5295e4a6426a2be5d3abeb3eecddfe12b5767002
Jakob Svemar <jakob_svemar@hotmail.com>
2016-04-08 10:40:59 +0200

    changed so that users keep track of appointments, not the other way around

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -1,26 +1,14 @@
 package model;
 
 import java.util.Date;
-import java.util.HashSet;
 
 public class Appointment implements Comparable<Appointment>{
 	private Date start;
 	private Date finish;
-	private HashSet<String> participants;
 	
 	public Appointment(Date start, Date finish, String id){
 		this.start = start;
 		this.finish = finish;
-		participants = new HashSet<String>();
-		participants.add(id);
-	}
-	
-	public boolean addParticipant(String id){
-		if(participants.contains(id))
-			return false;
-		
-		participants.add(id);
-		return true;
 	}
 	
 	public Date getStartDate(){
