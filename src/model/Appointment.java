28ebd47b056b1c10d907bd69e86c0f073d9d24b6
Jakob Svemar <jakob_svemar@hotmail.com>
2016-04-08 09:56:46 +0200

    added atrributes for User

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.util.Date;
+
 public class Appointment {
+	private Date start;
+	private Date finish;
 
 }
--- src/model/User.java
+++ src/model/User.java
@@ -3,5 +3,20 @@ package model;
 import java.util.ArrayList;
 
 public class User {
+	private String id;
+	private String name;
+	
+	public User (String id, String name){
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId(){
+		return id;
+	}
+	
+	public String getName(){
+		return name;
+	}
 
 }
