adf9a2cca1db4f99df7532583439f9119e8a2aba
Jakob Svemar <jakob_svemar@hotmail.com>
2016-04-14 17:41:39 +0200

    Adding controller package and launcher class

---
+++ src/controller/Launcher.java
@@ -0,0 +1,96 @@
+package controller;
+
+import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+import viewer.MeetingSuggesterGUI;
+import model.Appointment;
+import model.User;
+import model.UserDatabase;
+
+public class Launcher {
+	private static UserDatabase udb;
+	private MeetingSuggesterGUI window;
+	
+	public Launcher(){
+		udb = new UserDatabase();	
+	}
+
+	public void initializeGUI() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					window = new MeetingSuggesterGUI(udb);
+					window.visible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public static void main(String args[]){
+		Launcher l = new Launcher();
+		l.initializeDB();
+		l.initializeGUI();
+		
+	}
+
+	public void initializeDB(){
+		ArrayList<String[]> appointments = new ArrayList<String[]>();
+
+		//Adds the users to database, saves appointments for later, freebusy hard coded for ease of use.
+		try{
+			BufferedReader reader = new BufferedReader(new FileReader(new File("freebusy.txt")));
+			String s = reader.readLine();
+			while(s != null){
+				String[]elements = s.split(";");
+				
+				if(elements.length == 2){
+					udb.put(elements[0], new User(elements[0], elements[1]));
+				} else if (elements.length == 4){
+					appointments.add(elements);
+				}
+				s = reader.readLine();
+			}
+			reader.close();
+		} catch(FileNotFoundException ex) {
+			System.err.println("freebusy.txt could not be found.");
+			System.exit(1);
+		} catch(IOException ex) {
+			System.err.println("Error while reading from freebusy.txt");
+			System.exit(1);
+		}
+		
+		//Input the appointments for each user
+		SimpleDateFormat sdf = new SimpleDateFormat("M/d/yy h:mm:ss aa", Locale.US);
+		for(String[] s : appointments){
+			parseAppointment(sdf, s);
+		}
+	}
+
+	private void parseAppointment(SimpleDateFormat sdf, String[] s) {
+		Date start = null;
+		Date finish = null;
+		Appointment app = null;
+		try {
+			start = sdf.parse(s[1]);
+			finish = sdf.parse(s[2]);
+			app = new Appointment(start, finish);
+		} catch (ParseException e) {
+			return;
+		}
+		if(app != null){
+			udb.get(s[0]).addAppointment(app);
+		}
+	}
+}
